68d1fb503155a20c89b6f90c3026b4d8ef50507f
NNTruong <nhuttruong6496@gmail.com>
2022-06-24 11:36:37 +0700

    create theme

---
+++ src/util/CreateTheme.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2022 - KuKuLu Vietnam Limited
+ *
+ * @author  NNTruong / nhuttruong6496@gmail.com
+ */
+import {extendTheme} from 'native-base';
+const theme = extendTheme({
+  colors: {
+    // Add new color
+    primary: {
+      200: '#2190DE34',
+      500: '#2190DE',
+      600: '#2190DE',
+    },
+  },
+});
+export default theme;
